41ba25cd0793ec0ea2a98081e69779c5099c58c1
sppeeedd <qustaine@gmail.com>
2021-05-31 23:45:40 +0300

    Size change

--- Assets/Scripts/Ghost/GhostTeleport.cs
+++ Assets/Scripts/Ghost/GhostTeleport.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class GhostTeleport : MonoBehaviour
 {
-    [SerializeField] private float teleportMultiplier;
-    [SerializeField] private Joystick joystick;
-
-    private float timeBetweenClicks = 0.2f;
+    [SerializeField] private float teleportDistance;
 
     public void OnTeleportButtonDown()
     {
-        float xAxis = joystick.Horizontal;
-        float yAxis = joystick.Vertical;
-
         Vector3 pos = transform.position;
-        pos.x += xAxis * teleportMultiplier;
-        pos.y += yAxis * teleportMultiplier;
+        pos.y += teleportDistance;
         transform.position = pos;
     }
 }
